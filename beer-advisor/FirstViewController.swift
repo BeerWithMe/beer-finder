ff378211b350ec8d26ac6350ba55c815ff9ec4ac
Ben Nelson <s.benjamin.nelson@gmail.com>
2014-09-19 17:53:20 -0700

    Linked buttons, image, and slider with first view controller.

--- beer-advisor/FirstViewController.swift
+++ beer-advisor/FirstViewController.swift
@@ -9,11 +9,57 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    
+    let beerData = BeerData()
+    
+    @IBAction func DontKnow(sender: AnyObject) {
+        println("You clicked the 'don't know' button!")
+    }
+    
+    @IBAction func RecommendationButton(sender: AnyObject) {
+        println("You clicked 'recommendations' button!")
+    }
+    
+    @IBAction func submitratingbutton(sender: AnyObject) {
+        println("You clicked 'Submit!'")
+        beerData.cycleImage()
+    }
+    
+    @IBAction func ratingslider(sender: AnyObject) {
+        println("You slid!")
+    }
+    
+    @IBOutlet weak var questionnaireImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+//        // Do any additional setup after loading the view, typically from a nib.
+//        
+//        let baseURL = NSURL(string: "localhost:3000/")
+//        let sharedSession = NSURLSession.sharedSession()
+//        
+//        let downloadTask: NSURLSessionDownloadTask = sharedSession.downloadTaskWithURL(baseURL, completionHandler: { (location: NSURL!, response: NSURLResponse!, error: NSError!) -> Void in
+//            
+//            if error == nil {
+//                let dataObject = NSData(contentsOfURL: location)
+//                let testDictionary: NSDictionary = NSJSONSerialization.JSONObjectWithData(dataObject, options: nil, error: nil) as NSDictionary
+//                let testBeerData = BeerData(testDictionary: testDictionary)
+//                println(testBeerData)
+//                println("Hi guys!")
+//
+//            }
+//        })
+//        downloadTask.resume()
+        
+        let url = NSURL(string: "http://localhost:3000/")
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) {(data, response, error) in
+            println(NSString(data: data, encoding: NSUTF8StringEncoding))
+        }
+        
+        task.resume()
+        
+        }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
